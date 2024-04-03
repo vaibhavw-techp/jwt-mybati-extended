3562fcc03332ffb2c98346f58813e4bbf1e61db6
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-03 19:56:26 +0530

    Removed unwanted code and files for jwt security

--- src/main/java/com/demo/jwt/JwtMybatisApplication/config/jwt/SecurityConfig.java
+++ src/main/java/com/demo/jwt/JwtMybatisApplication/config/jwt/SecurityConfig.java
@@ -1,15 +1,16 @@
-package com.demo.jwt.JwtMybatisApplication.jwtsecurity.config;
+package com.demo.jwt.JwtMybatisApplication.config.jwt;
 
 
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -25,7 +26,8 @@ import javax.crypto.SecretKey;
 @EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
 
-    private String key = "0ddf5597e02d981f8803c4cc11f015a4e52679d706edb29b595d9e466def5bcf95273a3053ab5d97ee893c23e4023b912daefaade316406a33b7685d4d223dfa";
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     private static final String[] AUTH_WHITE_LIST = {
             "/v3/api-docs/**",
@@ -57,12 +59,12 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(stringToSecretKey(key)).build();
+        return NimbusJwtDecoder.withSecretKey(stringToSecretKey(secretKey)).macAlgorithm(MacAlgorithm.HS512).build();
     }
 
-    private SecretKey stringToSecretKey(String key) {
-        return new OctetSequenceKey.Builder(key.getBytes())
-                .algorithm(JWSAlgorithm.HS256)
+    private SecretKey stringToSecretKey(String secretKey) {
+        return new OctetSequenceKey.Builder(secretKey.getBytes())
+                .algorithm(JWSAlgorithm.HS512)
                 .build()
                 .toSecretKey();
     }
