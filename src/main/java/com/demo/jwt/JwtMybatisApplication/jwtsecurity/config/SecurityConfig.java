177e0fa195b348c6a08fd3aee6c57284be820559
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-03 17:50:49 +0530

    Added changes in JWT security config changes for nimbus

--- src/main/java/com/demo/jwt/JwtMybatisApplication/jwtsecurity/config/SecurityConfig.java
+++ src/main/java/com/demo/jwt/JwtMybatisApplication/jwtsecurity/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.demo.jwt.JwtMybatisApplication.jwtsecurity.config;
 
 
-import org.springframework.beans.factory.annotation.Value;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -17,13 +19,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.crypto.SecretKey;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
 
-    @Value("${jwt.secret}")
-    private String Secret_Key;
+    private String key = "0ddf5597e02d981f8803c4cc11f015a4e52679d706edb29b595d9e466def5bcf95273a3053ab5d97ee893c23e4023b912daefaade316406a33b7685d4d223dfa";
 
     private static final String[] AUTH_WHITE_LIST = {
             "/v3/api-docs/**",
@@ -37,7 +39,6 @@ public class SecurityConfig {
         return httpSecurity.authorizeHttpRequests(authorize ->
                 authorize
                     .requestMatchers(AUTH_WHITE_LIST).permitAll()
-                    .requestMatchers("/write/**").hasAuthority("SCOPE_write")
                     .anyRequest().authenticated()
             )
             .oauth2ResourceServer(configure -> configure.jwt(Customizer.withDefaults()))
@@ -45,19 +46,25 @@ public class SecurityConfig {
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(Secret_Key).build();
-    }
-
-    @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         final JwtGrantedAuthoritiesConverter gac = new JwtGrantedAuthoritiesConverter();
         gac.setAuthoritiesClaimName("Role");
         gac.setAuthorityPrefix("ROLE_");
-
         final JwtAuthenticationConverter jac = new JwtAuthenticationConverter();
-        jac.setJwtGrantedAuthoritiesConverter(gac); // Set JwtGrantedAuthoritiesConverter instance
+        jac.setJwtGrantedAuthoritiesConverter(gac);
         return jac;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withSecretKey(stringToSecretKey(key)).build();
+    }
+
+    private SecretKey stringToSecretKey(String key) {
+        return new OctetSequenceKey.Builder(key.getBytes())
+                .algorithm(JWSAlgorithm.HS256)
+                .build()
+                .toSecretKey();
     }
 
 
