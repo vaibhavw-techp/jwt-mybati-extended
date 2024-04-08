cd3e2e095814866f2ced3c266400b6f25bd6c036
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-08 12:32:24 +0530

    Use of transactional with unchecked exception for subject exists or not

--- src/main/java/com/demo/jwt/JwtMybatisApplication/exceptions/DuplicateResourceException.java
+++ src/main/java/com/demo/jwt/JwtMybatisApplication/exceptions/DuplicateResourceException.java
@@ -1,6 +1,6 @@
 package com.demo.jwt.JwtMybatisApplication.exceptions;
 
-public class DuplicateResourceException extends RuntimeException {
+public class DuplicateResourceException extends Exception {
 
     public DuplicateResourceException(String resourceName){
         super(resourceName + " already assigned !!");
